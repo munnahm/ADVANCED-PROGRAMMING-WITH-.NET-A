607d6841a9d7ad5b9c59bddeede1f4321a4d9690
munnahm <munnagalaxya7@gmail.com>
2022-03-03 23:53:03 +0600

    CV

---
+++ MyCV/MyCV/Controllers/studentController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyCV.Controllers
+{
+    public class studentController : Controller
+    {
+        // GET: student
+        public ActionResult registration()
+        {
+            return View();
+        }
+        public ActionResult login()
+        {
+            return View();
+        }
+    }
+}
